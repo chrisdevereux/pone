de72004ce01fbde14bb8e487f2433612951fe691
Chris Devereux <devereux.chris@gmail.com>
2015-11-26 09:26:56 +0000

    Reorganize project

--- src/core/index.js
+++
@@ -1,8 +0,0 @@
-import widget from './widget';
-import * as propTraits from './prop-traits';
-import * as picker from './picker';
-import * as state from './state';
-import {sharedRegistry as event} from './event-registry';
-
-export default widget;
-export {event, propTraits, picker, state};
